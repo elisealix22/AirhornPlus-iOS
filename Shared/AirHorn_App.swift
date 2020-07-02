cee67f4a9c09f95c85566ee98ebd4f05ee8f0b12
Elise Alix <elisealix22@github.com>
2020-07-02 13:23:48 -0400

    Initial Commit

---
+++ Shared/AirHorn_App.swift
@@ -0,0 +1,17 @@
+//
+//  AirHorn_App.swift
+//  Shared
+//
+//  Created by Elise Alix on 7/2/20.
+//
+
+import SwiftUI
+
+@main
+struct AirHorn_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
